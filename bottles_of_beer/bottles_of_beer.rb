7700dd91d08ffb5edf60043702a465833fa4cb5d
Andy Lampert <lamperta@gmail.com>
2015-06-11 17:49:24 -0600

    wip

--- bottles_of_beer/bottles_of_beer.rb
+++ bottles_of_beer/bottles_of_beer.rb
@@ -2,10 +2,21 @@
 
 bottle_count = 99
 
-99.times do
-  puts "#{bottle_count} bottles of beer on the wall, "
-  puts "#{bottle_count} bottles of beer."
+# 99.times do
+#   puts "#{bottle_count} bottles of beer on the wall, "
+#   puts "#{bottle_count} bottles of beer."
+#   puts "Take one down and pass it around "
+#   puts "#{bottle_count - 1} bottles of beer on the wall."
+#   bottle_count -= 1
+# end
+
+
+def count_bottles(number)
+  puts "#{number} bottles of beer on the wall, "
+  puts "#{number} bottles of beer."
   puts "Take one down and pass it around "
-  puts "#{bottle_count - 1} bottles of beer on the wall."
+  puts "#{number - 1} bottles of beer on the wall."
   bottle_count -= 1
 end
+
+count_bottles(99)
