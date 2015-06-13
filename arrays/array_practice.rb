fcf52e49ae7455b92f47b83b3de93a2ca4e20f59
Andy Lampert <lamperta@gmail.com>
2015-06-13 14:12:38 -0600

    add array_practice.rb and answers
    [#96733568]

---
+++ arrays/array_practice.rb
@@ -0,0 +1,32 @@
+#!/usr/bin/env ruby
+
+array = (1..10).to_a
+
+# When array_practice.rb is run, it should print out the following strings (use Array methods!)
+# 1...2...3...4...5...6...7...8...9...10...
+puts array.join('...') + '...'
+# T-10, 9, 8, 7, 6, 5, 4, 3, 2, 1...  BLASTOFF!
+puts "T-" + array.reverse.join(', ') + "...  BLASTOFF!"
+# The last element is 10
+puts "The last element is #{array.last}"
+# The first element is 1
+puts "The first element is #{array.first}"
+# The third element is 3
+puts "The third element is #{array[2]}"
+# The element with an index of 3 is 4
+puts "The element with an index of 3 is #{array[3]}"
+# The second from last element is 9
+puts "The second from last element is #{array[8]}"
+# The first four elements are '1, 2, 3, 4'
+puts "The first four elements are '#{array[0..3].join(', ')}'"
+# If we delete 5, 6 and 7 from the array, we're left with [1,2,3,4,8,9,10]
+array = array - array.slice!(4..6)
+puts "If we delete 5, 6 and 7 from the array, we're left with [#{array.join(',')}]"
+# If we add 5 at the beginning of the array, we're left with [5,1,2,3,4,8,9,10]
+puts "If we add 5 at the beginning of the array, we're left with [#{array.unshift(5).join(',')}]"
+# If we add 6 at the end of the array, we're left with [5,1,2,3,4,8,9,10,6]
+puts "If we add 6 at the end of the array, we're left with [#{array.push(6).join(',')}]"
+# Only the elements [9, 10] are > 8.
+puts "Only the elements #{array[6..7]} are > 8."
+# If we remove all the elements, then the length of the array is 0
+puts "If we remove all the elements, then the length of the array is #{array.clear.length}"
