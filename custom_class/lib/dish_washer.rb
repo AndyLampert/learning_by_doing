96859207ca91bc149755a3e86bdc2027b7830781
Andy Lampert <lamperta@gmail.com>
2015-07-15 18:12:08 -0600

    add custom class exercise
    [#96738830]

--- custom_class/lib/dish_washer.rb
+++ custom_class/lib/dish_washer.rb
@@ -1,15 +1,39 @@
 # dish washer
 class DishWasher
-  def initialize(plates)
-    @plates = plates
+  def initialize
+    @max_capacity = 0
+    @dishes = 0
   end
 
-  def wash_dishes(num_of_dishes)
-    num_of_dishes + ' clean dishes'
+  def max_capacity(number_of_dishes)
+    @max_capacity = number_of_dishes
+  end
+
+  def over_capacity?
+    true unless (@dishes > @max_capacity)
+  end
+
+  def washer_ready?
+    true if @dishes and not over_capacity?
+    'ready!'
+  end
+
+  def load_washer(number_of_plates)
+    @dishes += number_of_plates
+  end
+
+  def run_washer
+    washer_ready? and not over_capacity?
+    'suds...'
   end
 end
 
-# It should give you the same num of clean plates that you put it
-# It shouldn't clean dishes if you don't put in soap
-# You have to put a certain range of soap
-# If you put in too much soap, it overflows
+washer = DishWasher.new
+# my arbitrary washer has a max capacity of 30...
+puts washer.max_capacity(30)
+# put 10 dishes in the washer
+puts washer.load_washer(10)
+# check if the washer is ready
+puts washer.washer_ready?
+# run that washer
+puts washer.run_washer
